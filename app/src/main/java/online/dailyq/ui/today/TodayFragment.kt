9cf2335a1464d7ce9617eeda1ae4313dbbaa5512
Wonhui Roh <rwhui012@gmail.com>
2024-01-16 17:08:26 +0900

    feat(app): HttpURLConnection 으로 API 호출하기

--- app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
+++ app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import online.dailyq.databinding.FragmentTodayBinding
 import online.dailyq.ui.base.BaseFragment
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class TodayFragment : BaseFragment() {
 
@@ -19,6 +23,30 @@ class TodayFragment : BaseFragment() {
     ): View {
         _binding = FragmentTodayBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Thread {
+            val url = URL("http://10.0.2.2:5000/v1/hello-world")
+
+            val conn = url.openConnection() as HttpURLConnection
+            conn.connectTimeout = 5000
+            conn.readTimeout = 5000
+            conn.requestMethod = "GET"
+
+            conn.connect()
+
+            val reader = BufferedReader(InputStreamReader(conn.inputStream))
+            val questionBody = reader.readText()
+            reader.close()
+            conn.disconnect()
+
+            activity?.runOnUiThread {
+                binding.question.text = questionBody
+            }
+        }.start()
     }
 
     override fun onDestroyView() {
