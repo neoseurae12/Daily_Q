6170ef9dd48f5825ad8c65a86043bbce42073497
Wonhui Roh <rwhui012@gmail.com>
2024-01-16 15:31:37 +0900

    feat(app): 오늘의 질문 레이아웃 만들기

--- app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
+++ app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
@@ -1,6 +1,28 @@
 package online.dailyq.ui.today
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import online.dailyq.databinding.FragmentTodayBinding
 import online.dailyq.ui.base.BaseFragment
 
 class TodayFragment : BaseFragment() {
+
+    var _binding: FragmentTodayBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTodayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
