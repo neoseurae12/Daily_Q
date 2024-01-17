46f8f2759dc71aa3f9648e5c1a494460721c8a9c
Wonhui Roh <rwhui012@gmail.com>
2024-01-18 00:21:49 +0900

    feat: Gson 사용

--- app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
+++ app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.gson.Gson
+import online.dailyq.api.response.HelloWorld
 import online.dailyq.databinding.FragmentTodayBinding
 import online.dailyq.ui.base.BaseFragment
-import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -46,13 +47,25 @@ class TodayFragment : BaseFragment() {
             reader.close()
             conn.disconnect()
 
+            /* JSONObject 버전
             val json = JSONObject(questionBody)
             val message = json.getString("message")
             val date = json.getString("date")
+             */
+
+            // Gson 버전
+            val gson = Gson()
+            val helloWorld = gson.fromJson(questionBody, HelloWorld::class.java)
 
             activity?.runOnUiThread {
+                /* JSONObject 버전
                 binding.question.text = message
                 binding.date.text = date
+                 */
+
+                // Gson 버전
+                binding.question.text = helloWorld.message
+                binding.date.text = helloWorld.date
             }
         }.start()
     }
